e388b1c41cdbc9c0c3db5ac8cc1025cca7f8869b
ADITYA VERMA <adityav1304@gmail.com>
2024-03-25 23:16:37 +0530

    Update SponsorSection.js

--- src/components/home/sponsorsection/SponsorSection.js
+++ src/components/home/sponsorsection/SponsorSection.js
@@ -3,6 +3,7 @@ import "./SponsorSection.css";
 import { NavLink } from "react-router-dom";
 import srmLogo from "../sponsorsection/images/srm.svg"; 
 import dsbslogo from "../sponsorsection/images/dsbs.png";
+import acmlogo from "../sponsorsection/images/acm.png";
 const SponsorSection = (props) => {
   return (
     <>
@@ -23,15 +24,19 @@ const SponsorSection = (props) => {
           </div>
           <div className="container">
   <div className="row justify-content-center">
-    <div className="col-lg-5 text-center"> {/* Reduced column width to accommodate equal spacing */}
+    <div className="col-lg-4 text-center"> {/* Reduced column width to accommodate equal spacing */}
       <img src={srmLogo} alt="SRM Logo" className="srm-logo" style={{ height: "100px", marginTop: "20px" }} />
     </div>
-    <div className="col-lg-1"></div> {/* Added an empty column for equal spacing */}
-    <div className="col-lg-5 text-center"> {/* Reduced column width to accommodate equal spacing */}
+    <div className="col-lg-4"></div> {/* Added an empty column for equal spacing */}
+    <div className="col-lg-4 text-center"> {/* Reduced column width to accommodate equal spacing */}
       <img src={dsbslogo} alt="DSBS Logo" className="srm-logo" style={{ height: "100px", marginTop: "20px" }} />
+    </div>
+    <div className="col-lg-4 text-center"> {/* Reduced column width to accommodate equal spacing */}
+      <img src={acmlogo} alt="ACM Logo" className="srm-logo" style={{ height: "100px", marginTop: "-130px" }} /> {/* Adjusted margin-top */}
     </div>
   </div>
 </div>
+
         </div>
       </section>
     </>
