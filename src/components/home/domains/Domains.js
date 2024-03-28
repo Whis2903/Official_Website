94941ccb89402f3e42240668d18b510ed141e416
ADITYA VERMA <adityav1304@gmail.com>
2024-03-29 03:22:31 +0530

    a

--- src/components/home/domains/Domains.js
+++ src/components/home/domains/Domains.js
@@ -2,6 +2,10 @@
 
 import React from 'react';
 import './Domains.css';
+import RDLogo from "./images/rd.png"
+import Creatives from "./images/creatives.png"
+import WebApp from "./images/webapp.png"
+import Corp from "./images/corp.png"
 
 function Domains() {
   return (
@@ -14,29 +18,28 @@ function Domains() {
       </h2>
       <div className="domains-grid">
         <div className="domain-card">
-          <div className="logo">Logo</div> {/* Placeholder for logo */}
+          <div className="logo"><img src={RDLogo} className='image'/></div> {/* Placeholder for logo */}
           <h3>R&D</h3>
           <p>
             Research and Development domain involves exploring new ideas, technologies, and methodologies to drive innovation.
           </p>
-          <p>Instagram Followers : </p>
         </div>
         <div className="domain-card">
-          <div className="logo">Logo</div> {/* Placeholder for logo */}
+          <div className="logo"><img src={WebApp} className='image' /></div> {/* Placeholder for logo */}
           <h3>Web/App Dev</h3>
           <p>
           Developing and maintaining websites with intuitive user interface and seamless user experience along with precise server-side web application logic.
           </p>
         </div>
         <div className="domain-card">
-          <div className="logo">Logo</div> {/* Placeholder for logo */}
+          <div className="logo"><img src={Corp} className='image'/></div> {/* Placeholder for logo */}
           <h3>Corporate</h3>
           <p>
             Corporate domain deals with business strategies, management, and operations within organizations.
           </p>
         </div>
         <div className="domain-card">
-          <div className="logo">Logo</div> {/* Placeholder for logo */}
+          <div className="logo"><img src={Creatives} className='image'/></div> {/* Placeholder for logo */}
           <h3>Creatives</h3>
           <p>
             Creatives domain involves artistic and design-oriented work, including graphic design, UI/UX design, and multimedia production.
