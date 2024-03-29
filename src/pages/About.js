bede17ee51c57d4cefa9fb4e6e6a8839c5b24b8f
ADITYA VERMA <adityav1304@gmail.com>
2024-03-30 01:56:44 +0530

    a

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ function About() {
         <div className="container" style={{ marginTop: "-110px" }}>
           <div className="gbc1"></div>
           <div className="row justify-content-center text-center">
-            <div className="col-lg-10 text-center">
+            <div className="col-lg-10 text-center mobtitle">
               <h1 className="title">
                 About{" "}
                 <span style={{ color: "var(--secondary-color)",textShadow: "0 0 10px #57abd7, 0 0 20px #57abd7" }}>
