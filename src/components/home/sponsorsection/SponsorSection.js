63889eaaebd5edb0d60d586b39758b66b6952b66
ADITYA VERMA <adityav1304@gmail.com>
2024-03-30 01:54:48 +0530

    fixed mobile responsiveness

--- src/components/home/sponsorsection/SponsorSection.js
+++ src/components/home/sponsorsection/SponsorSection.js
@@ -8,7 +8,7 @@ const SponsorSection = (props) => {
   return (
     <>
       <section className="landing-section section h-70vh" style={{marginTop:"150px"}}>
-        <div className="container" style={{ marginTop: "-110px" }}>
+        <div className="container" style={{ marginTop: "-210px" }}>
           <div className="gbc1"></div>
           <div className="row justify-content-center text-center">
             <div className="col-lg-10 text-center">
