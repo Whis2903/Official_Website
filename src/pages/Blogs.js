696b622e5886fa255e4a7d18f73cd647a1a9fc67
ADITYA VERMA <adityav1304@gmail.com>
2024-04-02 18:23:30 +0530

    Update Blogs.js

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Blogcard from "../components/blogs/blogcard/Blogcard";
-import "../css/Blogs.css"
+import Tab from "../components/team/tabbar/Tab";
+import "../css/Blogs.css";
+
 function Blogs() {
   return (
     <>
@@ -9,14 +11,44 @@ function Blogs() {
           <div className="gbc1"></div>
           <div className="row justify-content-center text-center">
             <div className="col-lg-10 text-center mobtitle">
-              <h1 className="title mobblogtitle " style={{marginBottom:"40px"}}>
+              <h1 className="title mobblogtitle " style={{ marginBottom: "40px" }}>
                 Our{" "}
                 <span style={{ color: "var(--secondary-color)", textShadow: "0 0 10px #57abd7, 0 0 20px #57abd7" }}>
-                 Blogs
+                  Blogs
                 </span>{" "}
               </h1>
             </div>
           </div>
+          <ul className="nav nav-tabs justify-content-center" role="tablist" style={{ marginBottom: "35px" }}>
+            <Tab
+              className="nav-link tab-btn"
+              id="rd-tab"
+              targetID="#rd-tab-pane"
+              title="Research & Development"
+              selected={false}
+            />
+            <Tab
+              className="nav-link tab-btn active"
+              id="webapp-tab"
+              targetID="#web-app-tab-pane"
+              title="Web/App Development"
+              selected={false}
+            />
+            <Tab
+              className="nav-link tab-btn"
+              id="technical-tab"
+              targetID="#technical-tab-pane"
+              title="Blockchain"
+              selected={false}
+            />
+            <Tab
+              className="nav-link tab-btn"
+              id="misc-tab"
+              targetID="#misc-tab-pane"
+              title="Miscellaneous"
+              selected={false}
+            />
+          </ul>
           <div className="blog-card-container">
             <Blogcard
               imgSrc="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*AMm6E7rpmHT4ca30cIiY2Q.png"
@@ -27,7 +59,7 @@ function Blogs() {
             />
             <Blogcard
               imgSrc="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*_dluUkHWTPSVw10Z6LhAlQ.png"
-              title="GANs in Big Data Analytics and Data Science"
+              title="GANs in Big Data Analytics and Data Science"
               aboutblog="ADITYA VERMA"
               role="Chairperson"
               Mediumlink="https://medium.com/@adityaver/gans-in-big-data-analytics-and-data-science-95caa0cc2054"
