26ebedcd230a9969746ef3e3e09f4b55ab39b5f5
ADITYA VERMA <adityav1304@gmail.com>
2024-04-02 14:54:19 +0530

    a

--- src/components/err/Err.js
+++ src/components/err/Err.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../err/Err.css';
-
+import Errimage from "./img/404.svg"
 function Err() {
   return (
     <>
@@ -10,12 +10,10 @@ function Err() {
           <div className="row justify-content-center text-center">
             <div className="col-lg-10 text-center mobtitle">
               <h1 className="title head404">
+              <div className="Erlogo" style={{marginBottom:"20px"}}><img src={Errimage} className='er'/></div>
                 <span className="head404" style={{ color: "var(--secondary-color)", textShadow: "0 0 10px #57abd7, 0 0 15px #57abd7" }}>
-                  404
-                </span>{" "}<br />
-                <span className="pp404" >
-                  The page you are looking for does not exist
-                </span>{" "}
+                The page you are looking for does not exist{" "}
+                </span><br />
               </h1>
             </div>
           </div>
