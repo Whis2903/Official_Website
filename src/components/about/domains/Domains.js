8114f23aa837c340a2d1829ef1ac12357e9959b4
ADITYA VERMA <adityav1304@gmail.com>
2024-04-02 09:20:23 +0530

    a

---
+++ src/components/about/domains/Domains.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import './Domains.css';
+import RDLogo from "./images/rd.png"
+import Creatives from "./images/creatives.png"
+import WebApp from "./images/webapp.png"
+import Corp from "./images/corp.png"
+import "aos/dist/aos.css";
+import AOS from 'aos';
+
+function Domains() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Adjust the duration as needed
+      once: true, // Only animate once
+      easing: 'ease-in-out', // Easing type
+    });
+  }, []);
+
+  return (
+    <div className="domains-container">
+      <h2 className="section-title" style={{marginBottom:"35px"}}>
+        Explore our{" "}
+        <span style={{ color: "var(--secondary-color)" }}>
+          Domains
+        </span>{" "}
+      </h2>
+      <div className="domains-grid">
+        <div className="domain-card" data-aos="fade-up" data-aos-duration="1000">
+          <div className="logo"><img src={RDLogo} className='image'/></div> {/* Placeholder for logo */}
+          <h3>R&D</h3>
+          <p>
+            Research and Development domain involves exploring new ideas, technologies, and methodologies to drive innovation.
+          </p>
+        </div>
+        <div className="domain-card" data-aos="fade-up" data-aos-duration="1000">
+          <div className="logo"><img src={WebApp} className='image' /></div> {/* Placeholder for logo */}
+          <h3>Web/App Dev</h3>
+          <p>
+          Developing and maintaining websites with intuitive user interface and seamless user experience along with precise server-side web application logic.
+          </p>
+        </div>
+        <div className="domain-card" data-aos="fade-up" data-aos-duration="1000">
+          <div className="logo"><img src={Corp} className='image'/></div> {/* Placeholder for logo */}
+          <h3>Corporate</h3>
+          <p>
+            Corporate domain deals with business strategies, management, and operations within organizations.
+          </p>
+        </div>
+        <div className="domain-card" data-aos="fade-up" data-aos-duration="1000">
+          <div className="logo"><img src={Creatives} className='image'/></div> {/* Placeholder for logo */}
+          <h3>Creatives</h3>
+          <p>
+            Creatives domain involves artistic and design-oriented work, including graphic design, UI/UX design, and multimedia production.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Domains;
